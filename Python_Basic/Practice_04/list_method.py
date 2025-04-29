e049ae7858bbb0c3e1db133a7c081b7df1bb50a4
Jev-Verse <jevirahmat0@gmail.com>
2025-04-29 23:19:25 +0700

    Python Basic Day 4

---
+++ Python_Basic/Practice_04/list_method.py
@@ -0,0 +1,13 @@
+listed = [9, 3, 6, 1, 2, 5, 4, 8, 0, 7]
+
+# Using append
+listed.append(10)
+print(listed)
+
+# Using remove
+listed.remove(0)
+print(listed)
+
+# Using sort
+listed.sort()
+print(listed)

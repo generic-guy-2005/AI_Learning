675b95875cde9a2b6072e98ca1b17509b7fb242c
Jev-Verse <jevirahmat0@gmail.com>
2025-04-29 23:10:57 +0700

    Python Basic #04

---
+++ Python_Basic/Practice_04/list.py
@@ -0,0 +1,14 @@
+# Creating a list
+listOfNum = [20, 45, 12, 34, 37, 43, 25, 17]
+
+# Access a specific values
+print(listOfNum[3])
+
+# Change the values
+print(listOfNum[4])
+listOfNum[4] = 22
+print(listOfNum[4])
+
+# Iterating over the list
+for number in listOfNum:
+    print(number)
